9616a2fda354d5a19399a3b7f87c93a21535b8bb
unknown <gaoxl@TECH.BITAUTO.COM>
2016-06-27 20:52:48 +0800

    add

--- task.sh
+++ task.sh
@@ -13,7 +13,7 @@ closured(){
    read ids name files class method params runtime interval start_time end_time flag <<< `echo "${task_arr[$1]}" | awk -F'###' '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}'`
    local vals="${ids}###${name}###${files}###${class}###${method}###${params}###${runtime}###${interval}###${start_time}###${now_time}###${run_start}"
    task_arr[$1]=$vals
-   echo -en $vals | $REDISEXEC -x set $2 
+   echo -en $vals | $REDISEXEC -x set $2 &>/dev/null
 }
 
 while true
@@ -28,7 +28,7 @@ do
         if [[ $runtime -le $now_time ]] && [[ $space_time -ge $interval ]] && [[ $flag -eq $run_start ]]; then
                 vals="${ids}###${name}###${files}###${class}###${method}###${params}###${runtime}###${interval}###${now_time}###${end_time}###${run_stop}"
                 task_arr[$ids]=$vals
-                echo -en $vals | $REDISEXEC -x set $keys 
+                echo -en $vals | $REDISEXEC -x set $keys &>/dev/null
                 $PHPEXEC $files $class $method $params && closured $ids $keys &
         fi
   done
