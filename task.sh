67ca9ba58881b16ea858e24515a944e3a32d5fcd
unknown <gaoxl@TECH.BITAUTO.COM>
2016-06-28 10:26:59 +0800

    add

--- task.sh
+++ task.sh
@@ -10,8 +10,8 @@ if [ ! -e $access_file ];then
 fi
 closured(){
    local now_time=`date "+%s"`
-   read ids name files class method params runtime interval start_time end_time flag <<< `echo "${task_arr[$1]}" | awk -F'###' '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}'`
-   local vals="${ids}###${name}###${files}###${class}###${method}###${params}###${runtime}###${interval}###${start_time}###${now_time}###${run_start}"
+   read ids name group files class method params runtime interval start_time end_time flag status remarks <<< `echo "${task_arr[$1]}" | awk -F'###' '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14}'`
+   local vals="${ids}###${name}###$group###${files}###${class}###${method}###${params}###${runtime}###${interval}###${now_time}###${end_time}###${run_stop}###${status}###${remarks}"
    task_arr[$1]=$vals
    echo -en $vals | $REDISEXEC -x set $2 &>/dev/null
 }
@@ -23,15 +23,15 @@ do
         #当前时间
         now_time=`date "+%s"`
         data=`$REDISEXEC get $keys`
-        read ids name files class method params runtime interval start_time end_time flag <<< `echo $data | awk -F'###' '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}'`
+        read ids name group files class method params runtime interval start_time end_time flag status remarks <<< `echo $data | awk -F'###' '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14}'`
         space_time=`expr ${now_time} - ${end_time}`
-        if [[ $runtime -le $now_time ]] && [[ $space_time -ge $interval ]] && [[ $flag -eq $run_start ]]; then
-                vals="${ids}###${name}###${files}###${class}###${method}###${params}###${runtime}###${interval}###${now_time}###${end_time}###${run_stop}"
+        if [[ $runtime -le $now_time ]] && [[ $space_time -ge $interval ]] && [[ $flag -eq $run_start ]] && [[ $status -eq 1 ]]; then
+                vals="${ids}###${name}###$group###${files}###${class}###${method}###${params}###${runtime}###${interval}###${now_time}###${end_time}###${run_stop}###${status}###${remarks}"
                 task_arr[$ids]=$vals
                 echo -en $vals | $REDISEXEC -x set $keys &>/dev/null
                 $PHPEXEC $files $class $method $params && closured $ids $keys &
         fi
-  done
-  sleep 1
+    done
+    sleep 1
 done
 exit 0
