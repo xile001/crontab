5d7713d311e345dc65a03de3aeba0c3f54d112cf
xile <gaoxl@cig.com.cn>
2017-01-16 10:50:45 +0800

    add

--- task.sh
+++ task.sh
@@ -5,10 +5,11 @@
 readonly run_start=1
 readonly run_stop=2
 readonly step=1
+pro_time=`date "+%s"`
 
 closured(){
-  local now_time=`date "+%s"`
-  $REDISEXEC hmset $1 end "$now_time" flag "$run_start" &>/dev/null
+  local n_time=`date "+%s"`
+  $REDISEXEC hmset $1 endtime "$n_time" flag "$run_start" &>/dev/null
 }
 subtask(){
   local -a arrs
@@ -24,13 +25,13 @@ subtask(){
   local suffix=''
   local num=0
   local run_status=0
-  local interval=0
-  local now_time=`date "+%s"`
+  local n_time=0
   local pid=${1##*p}
   #有序集合
-  for key in `$REDISEXEC zrange ${REDIS_KEYS}${ip32_addr}zps_${pid} 0 -1`
+  local zps_rediskey="${REDIS_KEYS}${ip32_addr}zps_${pid}"
+  for key in `$REDISEXEC zrange $zps_rediskey 0 -1`
   do
-    arrs=(`$REDISEXEC hmget $key ids name group classify files class method params flag status nums interval_seconds`)
+    arrs=(`$REDISEXEC hmget $key ids name group classify files class method params flag status nums`)
     ids=${arrs[0]}
     name=${arrs[1]}
     group=${arrs[2]}
@@ -42,18 +43,17 @@ subtask(){
     flag=${arrs[8]}
     run_status=${arrs[9]}
     num=${arrs[10]}
-    interval=${arrs[11]}
     suffix=${files##*.}
     if [[ $flag -eq $run_start && $run_status -eq 1 && -f $files ]];then
       num=$[num + 1]
-      now_time=`date "+%s"`
-      $REDISEXEC hmset $key start "$now_time" nums "$num" flag "$run_stop" &>/dev/null
+      n_time=`date "+%s"`
+      $REDISEXEC hmset $key start "$n_time" nums "$num" flag "$run_stop" &>/dev/null
       case $params in
         0)
-          params="tid:s$ids tinterval:s$interval tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
+          params="tid:s$ids tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
           ;;
         *)
-          params="$params tid:s$ids tinterval:s$interval tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
+          params="$params tid:s$ids tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
       esac
       case $suffix in
         php)
@@ -68,18 +68,19 @@ subtask(){
       esac
     fi
   done
-  now_time=`date "+%s"`
-  $REDISEXEC hmset $1 end "$now_time" flag "$run_start" &>/dev/null
+  n_time=`date "+%s"`
+  $REDISEXEC hmset $1 endtime "$n_time" flag "$run_start" &>/dev/null
 }
 
 while true
 do
-  for keys in `$REDISEXEC zrevrange ${REDIS_KEYS}${ip32_addr}zp 0 -1`
+  zp_rediskey="${REDIS_KEYS}${ip32_addr}zp"
+  for keys in `$REDISEXEC zrevrange $zp_rediskey 0 -1`
   do
     #当前时间
     now_time=`date "+%s"`
     now_date_arr=(`date "+%Y %m %u %d %H %M %S"`)
-    arr=(`$REDISEXEC hmget $keys ids name group classify files class method params int_runtime year month week day hour minute second interval_seconds start end flag status nums`)
+    arr=(`$REDISEXEC hmget $keys ids name group classify files class method params int_runtime year month week day hour minute second interval_seconds starttime endtime flag status nums`)
     ids=${arr[0]}
     name=${arr[1]}
     group=${arr[2]}
@@ -118,7 +119,6 @@ do
         if [[ $interval -eq 0 || $space_time -ge $interval || $end_time -gt $delayed_time ]];then
           state=1
         fi
-        interval="s$interval"
         ;;
       2)
         #定时
@@ -126,147 +126,86 @@ do
           state=0
         elif [[ $year == ${now_date_arr[0]} ]];then
           state=1
-          interval="${year}y"
         else
           state=2
         fi
 
         if [[ $state -eq 1 ]];then
-          if [[ $month == ${now_date_arr[1]} ]];then
-            state=1
-            interval="${interval}m$month"
-          else
+          if [[ $month != ${now_date_arr[1]} && $month != '00' ]];then
             state=0
+          elif [[ $month == ${now_date_arr[1]} ]];then
+            state=1
+          else
+            state=1
           fi
         elif [[ $state -eq 2 ]];then
           if [[ $month != ${now_date_arr[1]} && $month != '00' ]];then
             state=0
           elif [[ $month == ${now_date_arr[1]} ]];then
             state=1
-            interval="${interval}m$month"
           else
             state=2
           fi
-        else
-          state=0
         fi
 
         if [[ $state -eq 1 ]];then
-          if [[ $week == ${now_date_arr[2]} ]];then
-            state=1
-            interval="${interval}w$week"
-          else
+          if [[ $week != ${now_date_arr[2]} && $week != '0' ]];then
             state=0
+          elif [[ $week == ${now_date_arr[2]} ]];then
+            state=1
+          else
+            if [[ $day != ${now_date_arr[3]} && $day != '00' ]];then
+              state=0
+            elif [[ $day == ${now_date_arr[3]} ]];then
+              state=1
+            else
+              state=1
+            fi
           fi
         elif [[ $state -eq 2 ]];then
           if [[ $week != ${now_date_arr[2]} && $week != '0' ]];then
             state=0
           elif [[ $week == ${now_date_arr[2]} ]];then
             state=1
-            interval="${interval}w$week"
           else
-            state=2
+            if [[ $day != ${now_date_arr[3]} && $day != '00' ]];then
+              state=0
+            elif [[ $day == ${now_date_arr[3]} ]];then
+              state=1
+            else
+              state=2
+            fi
           fi
-        else
-          state=0
         fi
 
-        if [[ $state -eq 1 ]];then
-          if [[ $day == ${now_date_arr[3]} ]];then
+        if [[ $state -eq 1 || $state -eq 2 ]];then
+          if [[ $hour == ${now_date_arr[4]} && $minute == ${now_date_arr[5]} && $second == ${now_date_arr[6]} ]];then
             state=1
-            interval="${interval}d$day"
           else
-            state=0
+            s_time=`date +%s -d "${now_date_arr[0]}${now_date_arr[1]}${now_date_arr[3]} ${hour}:${minute}:${second}"`
+            c_time=$[now_time - pro_time]
+            if [[ $c_time -gt 1 && $s_time -gt $pro_time && $s_time -lt $now_time ]];then
+              state=1
+            else
+              state=0
+            fi
           fi
-        elif [[ $state -eq 2 ]];then
-          if [[ $day != ${now_date_arr[3]} && $day != '00' ]];then
-            state=0
-          elif [[ $day == ${now_date_arr[3]} ]];then
-            state=1
-            interval="${interval}d$day"
-          else
-            state=2
-          fi
-        else
-          state=0
-        fi
-
-        if [[ $state -eq 1 ]];then
-          if [[ $hour == ${now_date_arr[4]} ]];then
-            state=1
-            interval="${interval}h$hour"
-          else
-            state=0
-          fi
-        elif [[ $state -eq 2 ]];then
-          if [[ $hour != ${now_date_arr[4]} && $hour != '00' ]];then
-            state=0
-          elif [[ $hour == ${now_date_arr[4]} ]];then
-            state=1
-            interval="${interval}h$hour"
-          else
-            state=2
-          fi
-        else
-          state=0
-        fi
-
-        if [[ $state -eq 1 ]];then
-          if [[ $minute == ${now_date_arr[5]} ]];then
-            state=1
-            interval="${interval}i$minute"
-          else
-            state=0
-          fi
-        elif [[ $state -eq 2 ]];then
-          if [[ $minute != ${now_date_arr[5]} && $minute != '00' ]];then
-            state=0
-          elif [[ $minute == ${now_date_arr[5]} ]];then
-            state=1
-            interval="${interval}i$minute"
-          else
-            state=2
-          fi
-        else
-          state=0
-        fi
-
-        if [[ $state -eq 1 ]];then
-          if [[ $second == ${now_date_arr[6]} ]];then
-            state=1
-            interval="${interval}s$second"
-          else
-            state=0
-          fi
-        elif [[ $state -eq 2 ]];then
-          if [[ $second != ${now_date_arr[6]} && $second != '00' ]];then
-            state=0
-          elif [[ $second == ${now_date_arr[6]} ]];then
-            state=1
-            interval="${interval}s$second"
-          elif [[ $year == '0000' && $month == '00' && $week == '0' && $day == '00' && $hour == '00' && $minute == '00' && $second == '00' ]];then
-            state=1
-            interval="${interval}s$second"
-          else
-            state=2
-          fi
-        else
-          state=0
         fi
         ;;
     esac
+    pro_time=$now_time
     #echo `date "+%Y %m %u %d %H %M %S"`
-    #echo "$classify $interval $int_runtime $now_time $state $flag $run_start $run_status $files"
-    if [[ $int_runtime -le $now_time && $state -eq 1 && $flag -eq $run_start && -f $files ]];then
+    #echo "$ids $classify $int_runtime $now_time $state $flag $run_start $run_status $files"
+    if [[ $state -eq 1 && $flag -eq $run_start && $int_runtime -le $now_time && -f $files ]] || [[ $int_runtime -le $now_time && $num -eq 0 ]];then
       num=$[num + 1]
       now_time=`date "+%s"`
-      $REDISEXEC hmset $keys start "$now_time" nums "$num" flag "$run_stop" &>/dev/null
+      $REDISEXEC hmset $keys starttime "$now_time" nums "$num" flag "$run_stop" &>/dev/null
       case $params in
         0)
-          params="tid:p$ids tinterval:$interval tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
+          params="tid:p$ids tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
           ;;
         *)
-          params="$params tid:p$ids tinterval:$interval tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
+          params="$params tid:p$ids tgroup_name:$group tclassify:$classify tname:$name ip32_addr:$ip32_addr"
       esac
       case $suffix in
         php)
@@ -279,7 +218,7 @@ do
           $JAVAEXEC $files $class $method $params && subtask $keys &
           ;;
         *)
-          echo "php or sh"
+          echo "php or sh or java"
           exit 1
       esac
     fi
